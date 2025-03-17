4496715e1da3fa630dde50cc5e988ceae99241c0
Kevin Bertin <kevinbertinpro@gmail.com>
2025-03-17 14:45:46 +0100

    Adding price to form card

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,8 +12,8 @@ export const Button = ({ textContent }: ButtonProps) => {
             className="relative flex flex-row items-center justify-center group w-80"
         >
             <div className="bg-white w-12 h-12 flex items-center rounded-full group-hover:w-full absolute top-0 left-0 translate-y-1/2 transition-all duration-700">
-            <FaArrowRightLong className="text-[#0F1921] z-50 absolute left-6 -translate-x-1/2 group-hover:left-7 transition-all duration-300" />
-            <p className="absolute left-16 uppercase text-nowrap font-semibold group-hover:bg-transparent group-hover:text-[#0F1921] text-white transition-all duration-300">
+            <FaArrowRightLong className="text-black/70 z-50 absolute left-6 -translate-x-1/2 group-hover:left-7 transition-all duration-300" />
+            <p className="absolute left-16 uppercase text-nowrap font-semibold group-hover:bg-transparent group-hover:text-black/60 text-white transition-all duration-300">
                 {textContent}
             </p>
             </div>
--- app/components/CustomerForm.tsx
+++ app/components/CustomerForm.tsx
@@ -89,150 +89,188 @@ export const CustomerForm = () => {
     };
 
     return (
-        <form
-            ref={form}
-            onSubmit={recordCustomer}
-            className="flex flex-col gap-4 backdrop-blur-lg bg-white/5 rounded-3xl shadow-xl p-8"
-        >
-            <div className="flex flex-col gap-2">
-                <p>Vous êtes: </p>
-                <div className="flex gap-4">
-                    <button
-                        type="button"
-                        className={`py-2 px-8 rounded-2xl ${
-                            customerType === "particulier"
-                                ? "bg-white text-[#0F1921]"
-                                : "bg-transparent border-white border-2 text-white"
-                        }`}
-                        onClick={() => setCustomerType("particulier")}
-                    >
-                        un particulier
-                    </button>
-                    <button
-                        type="button"
-                        className={`py-2 px-8 rounded-2xl ${
-                            customerType === "structure"
-                                ? "bg-white text-[#0F1921]"
-                                : "bg-transparent border-white border-2 text-white"
-                        }`}
-                        onClick={() => setCustomerType("structure")}
-                    >
-                        une structure
-                    </button>
-                </div>
-            </div>
-
-            <motion.div
-                key={customerType} // Change de clé à chaque changement d'état, forçant une réanimation
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                transition={{ duration: 0.5 }}
-                className="flex flex-col gap-2"
+        <div className="flex gap-8 backdrop-blur-lg bg-white/5 rounded-3xl shadow-xl p-8">
+            <form
+                ref={form}
+                onSubmit={recordCustomer}
+                className="flex flex-col gap-4 "
             >
-                {customerType === "particulier" ? (
-                    <>
-                        <div className="flex flex-col md:flex-row gap-2 md:gap-12">
-                            <CustomerInput
-                                name="lastName"
-                                type="text"
-                                onChange={handleChange}
-                                label="Nom :"
-                                required
-                            />
-                            <CustomerInput
-                                name="firstName"
-                                type="text"
-                                onChange={handleChange}
-                                label="Prénom :"
-                                required
-                            />
-                        </div>
-                        <div className="flex flex-col md:flex-row gap-2 md:gap-12">
-                            <div className="flex flex-col gap-2">
-                                <label htmlFor="age">Âge: </label>
-                                <select
-                                    name="age"
-                                    id="age"
-                                    className="text-black px-4 p-2 rounded-full md:w-64 appearance-none"
-                                    defaultValue=""
-                                    onChange={handleChange}
-                                    required
-                                >
-                                    <option value="" disabled hidden></option>
-                                    <option value="minor">
-                                        Moins de 18 ans
-                                    </option>
-                                    <option value="youngAdult">
-                                        18-30 ans
-                                    </option>
-                                    <option value="olderAdult">
-                                        30-45 ans
-                                    </option>
-                                    <option value="elder">
-                                        Plus de 45 ans
-                                    </option>
-                                </select>
-                            </div>
-                            <CustomerInput
-                                name="city"
-                                type="text"
-                                onChange={handleChange}
-                                label="Ville de résidence :"
-                                required
-                            />
-                        </div>
-                    </>
-                ) : (
-                    <>
-                        <div className="flex flex-col md:flex-row gap-12">
-                            <CustomerInput
-                                name="companyName"
-                                type="text"
-                                onChange={handleChange}
-                                label="Nom de la structure :"
-                                required
-                            />
-                            <CustomerInput
-                                name="city"
-                                type="text"
-                                onChange={handleChange}
-                                label="Ville :"
-                                required
-                            />
-                        </div>
-                    </>
-                )}
-                <div className="flex flex-col md:flex-row gap-2 md:gap-12">
-                    <CustomerInput
-                        name="email"
-                        type="email"
-                        onChange={handleChange}
-                        label="Adresse e-mail :"
-                        required
-                    />
-                    <CustomerInput
-                        name="phoneNumber"
-                        type="tel"
-                        onChange={handleChange}
-                        label="Numéro de téléphone :"
-                        required={false}
-                    />
+                <div className="flex flex-col gap-2">
+                    <p>Vous êtes: </p>
+                    <div className="flex gap-4">
+                        <button
+                            type="button"
+                            className={`py-2 px-8 rounded-2xl ${
+                                customerType === "particulier"
+                                    ? "bg-white text-[#0F1921]"
+                                    : "bg-transparent border-white border-2 text-white"
+                            }`}
+                            onClick={() => setCustomerType("particulier")}
+                        >
+                            un particulier
+                        </button>
+                        <button
+                            type="button"
+                            className={`py-2 px-8 rounded-2xl ${
+                                customerType === "structure"
+                                    ? "bg-white text-[#0F1921]"
+                                    : "bg-transparent border-white border-2 text-white"
+                            }`}
+                            onClick={() => setCustomerType("structure")}
+                        >
+                            une structure
+                        </button>
+                    </div>
                 </div>
-                <div className="flex flex-col md:flex-row gap-2 md:gap-12 items-baseline">
-                    <div className="flex flex-col gap-2">
-                        <label htmlFor="message">Message :</label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            placeholder="Ecrivez vos demandes particulières ici."
+
+                <motion.div
+                    key={customerType} // Change de clé à chaque changement d'état, forçant une réanimation
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -10 }}
+                    transition={{ duration: 0.5 }}
+                    className="flex flex-col gap-2"
+                >
+                    {customerType === "particulier" ? (
+                        <>
+                            <div className="flex flex-col md:flex-row gap-2 md:gap-12">
+                                <CustomerInput
+                                    name="lastName"
+                                    type="text"
+                                    onChange={handleChange}
+                                    label="Nom :"
+                                    required
+                                />
+                                <CustomerInput
+                                    name="firstName"
+                                    type="text"
+                                    onChange={handleChange}
+                                    label="Prénom :"
+                                    required
+                                />
+                            </div>
+                            <div className="flex flex-col md:flex-row gap-2 md:gap-12">
+                                <div className="flex flex-col gap-2">
+                                    <label htmlFor="age">Âge: </label>
+                                    <select
+                                        name="age"
+                                        id="age"
+                                        className="text-black px-4 p-2 rounded-full md:w-64 appearance-none"
+                                        defaultValue=""
+                                        onChange={handleChange}
+                                        required
+                                    >
+                                        <option
+                                            value=""
+                                            disabled
+                                            hidden
+                                        ></option>
+                                        <option value="minor">
+                                            Moins de 18 ans
+                                        </option>
+                                        <option value="youngAdult">
+                                            18-30 ans
+                                        </option>
+                                        <option value="olderAdult">
+                                            30-45 ans
+                                        </option>
+                                        <option value="elder">
+                                            Plus de 45 ans
+                                        </option>
+                                    </select>
+                                </div>
+                                <CustomerInput
+                                    name="city"
+                                    type="text"
+                                    onChange={handleChange}
+                                    label="Ville de résidence :"
+                                    required
+                                />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="flex flex-col md:flex-row gap-12">
+                                <CustomerInput
+                                    name="companyName"
+                                    type="text"
+                                    onChange={handleChange}
+                                    label="Nom de la structure :"
+                                    required
+                                />
+                                <CustomerInput
+                                    name="city"
+                                    type="text"
+                                    onChange={handleChange}
+                                    label="Ville :"
+                                    required
+                                />
+                            </div>
+                        </>
+                    )}
+                    <div className="flex flex-col md:flex-row gap-2 md:gap-12">
+                        <CustomerInput
+                            name="email"
+                            type="email"
                             onChange={handleChange}
-                            className="text-black px-4 p-2 rounded-2xl md:w-64"
+                            label="Adresse e-mail :"
+                            required
+                        />
+                        <CustomerInput
+                            name="phoneNumber"
+                            type="tel"
+                            onChange={handleChange}
+                            label="Numéro de téléphone :"
+                            required={false}
                         />
                     </div>
-                    <Button textContent="Soumettre le formulaire" />
+                    <div className="flex flex-col md:flex-row gap-2 md:gap-12 items-baseline">
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="message">Message :</label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                placeholder="Ecrivez vos demandes particulières ici."
+                                onChange={handleChange}
+                                className="text-black px-4 p-2 rounded-2xl md:w-64"
+                            />
+                        </div>
+                        <Button textContent="Soumettre le formulaire" />
+                    </div>
+                </motion.div>
+            </form>
+            {customerType === "particulier" ? (
+                <div className="flex flex-col gap-2 justify-center">
+                    <h3 className="text-2xl font-semibold">
+                        Les ateliers individuels
+                    </h3>
+                    <p className="text-justify font-light">
+                        Les ateliers individuels sont facturés{" "}
+                        <span className="font-bold">35€/heure</span> par atelier
+                        sur la base d’ateliers de 2 heures. <br />
+                        En fonction du lieu de déplacement ce tarif peut être
+                        amené à être augmenté ou réduit.
+                    </p>
+                    <div className="flex w-full gap-2 items-end">
+                        <p className="italic text-sm">Sur devis.</p>
+                        <span className="text-5xl font-bold">35€/h.</span>
+                    </div>
                 </div>
-            </motion.div>
-        </form>
+            ) : (
+                <div className="flex flex-col gap-2 justify-center">
+                    <h3 className="text-2xl font-semibold">
+                        Les ateliers collectifs
+                    </h3>
+                    <p className="text-justify font-light">
+                        Les ateliers collectifs sont facturés après évaluation
+                        du projet avec la structure, en fonction des objectifs.
+                    </p>
+                    <div className="flex w-full gap-2 items-end">
+                        <p className="italic text-sm">Sur devis.</p>
+                        <span className="text-5xl font-bold">--€/h.</span>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
